b35df6e2367d6e2be6c38fc17e37bac8ad0154f7
Roman Burko <redstone.kem@gmail.com>
2022-07-20 20:38:46 +0300

    new content src/Games/Calc.php

---
+++ src/Games/Calc.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace BrainGames\Calc;
+
+use function cli\line;
+use function cli\prompt;
+use function BrainGames\Lib\generateExpression;
+use function BrainGames\Lib\calculateExpression;
+use function BrainGames\Lib\commentTheAnswer;
+
+line('What is the result of the expression?');
+while (($score < $goal) && ($fault === false)) {
+    [$num1, $num2, $operation] = generateExpression();
+    line("Question: %s %s %s", $num1, $operation, $num2);
+    $userAnswer = prompt("Your answer");
+    $correctAnswer = calculateExpression($num1, $num2, $operation);
+    $result = ($userAnswer === $correctAnswer);
+    $result ? $score++ : $fault = true;
+    commentTheAnswer($result, $userAnswer, $correctAnswer);
+}
