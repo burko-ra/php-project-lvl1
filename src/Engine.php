ea66d0a082dfeb26187e3063e074445feeb95b12
Roman Burko <redstone.kem@gmail.com>
2022-07-22 15:52:39 +0300

    fix: src/Engine greeting line

--- src/Engine.php
+++ src/Engine.php
@@ -10,7 +10,7 @@ use function BrainGames\GCD\playBrainGCD;
 use function BrainGames\Progression\playBrainProgression;
 use function BrainGames\Prime\playBrainPrime;
 
-line('Welcome to the Brain Game!');
+line('Welcome to the Brain Games!');
 $name = prompt('May I have your name?');
 line("Hello, %s!", $name);
 
