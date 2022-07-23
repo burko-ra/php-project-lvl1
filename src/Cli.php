7e4e1565edc38b720adc37abab644c9aea8531c7
Roman Burko <redstone.kem@gmail.com>
2022-07-23 15:51:58 +0300

    fix: logic in bin/brain-games

--- src/Cli.php
+++ src/Cli.php
@@ -5,6 +5,9 @@ namespace BrainGames\Cli;
 use function cli\line;
 use function cli\prompt;
 
-line('Welcome to the Brain Game!');
-$name = prompt('May I have your name?');
-line("Hello, %s!", $name);
+function welcome()
+{
+    line('Welcome to the Brain Game!');
+    $name = prompt('May I have your name?');
+    line("Hello, %s!", $name);
+}
