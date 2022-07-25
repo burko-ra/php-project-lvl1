43717fe4c53607ee068f6bd02c446bbe96dc52c8
Roman Burko <redstone.kem@gmail.com>
2022-07-25 14:16:00 +0300

    refactor: src/Games/Calc

--- src/Games/Calc.php
+++ src/Games/Calc.php
@@ -6,20 +6,25 @@ use function BrainGames\Engine\play;
 
 use const BrainGames\Engine\ROUND_MAX;
 
-function playBrainCalc()
+function makeExpression(): array
 {
-    $questionLine = 'What is the result of the expression?';
     $randMin = 1;
     $randMax = 10;
     $listOperations = ['+', '-', '*'];
+    $num1 = rand($randMin, $randMax);
+    $num2 = rand($randMin, $randMax);
+    $numOperation = rand(0, count($listOperations) - 1);
+    $operation = $listOperations[$numOperation];
+    return [$num1, $num2, $operation];
+}
+
+function playBrainCalc()
+{
+    $questionLine = 'What is the result of the expression?';
 
     $gameData = [];
     for ($i = 0; $i < ROUND_MAX; $i++) {
-        $num1 = rand($randMin, $randMax);
-        $num2 = rand($randMin, $randMax);
-        $numOperation = rand(0, count($listOperations) - 1);
-        $operation = $listOperations[$numOperation];
-
+        [$num1, $num2, $operation] = makeExpression();
         switch ($operation) {
             case '+':
                 $result = $num1 + $num2;
